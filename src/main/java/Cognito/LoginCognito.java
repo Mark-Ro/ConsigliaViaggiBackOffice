87a1da357b078f4a31bfeb63bace336f2be1657e
Mark-Ro <marco.romano9898@gmail.com>
2020-04-28 15:18:30 +0200

    Implementato login admin

---
+++ src/main/java/Cognito/LoginCognito.java
@@ -0,0 +1,34 @@
+package Cognito;
+
+import com.amazonaws.auth.*;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProvider;
+import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProviderClientBuilder;
+import com.amazonaws.services.cognitoidp.model.AdminInitiateAuthRequest;
+import com.amazonaws.services.cognitoidp.model.AdminInitiateAuthResult;
+import com.amazonaws.services.cognitoidp.model.AuthFlowType;
+import com.amazonaws.services.cognitoidp.model.NotAuthorizedException;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+
+public class LoginCognito {
+
+    public void doLoginAdmin(String username, String password) throws InvocationTargetException, NotAuthorizedException {
+        String userPoolId="eu-central-1_lU4xtGPPe";
+        String amazonAWSAccessKey="AKIAJYPRFLV3MFY3NPIQ";
+        String amazonAWSSecretKey="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";
+        String clientId="5jhst4j81qn2tkt6u2ojfljle";
+
+        HashMap authParameters = new HashMap<>();
+        authParameters.put("USERNAME",username);
+        authParameters.put("PASSWORD",password);
+        AdminInitiateAuthRequest req = new AdminInitiateAuthRequest().withClientId(clientId).withUserPoolId(userPoolId).withAuthFlow(AuthFlowType.ADMIN_NO_SRP_AUTH).withAuthParameters(authParameters);
+        AWSCredentials credentials = new BasicAWSCredentials(amazonAWSAccessKey, amazonAWSSecretKey);
+        AWSCredentialsProvider credentialsProvider = new AWSStaticCredentialsProvider(credentials);
+        credentialsProvider.getCredentials();
+        req.setRequestCredentialsProvider(credentialsProvider);
+        AWSCognitoIdentityProvider provider = AWSCognitoIdentityProviderClientBuilder.standard().withCredentials(DefaultAWSCredentialsProviderChain.getInstance()).withRegion(Regions.EU_CENTRAL_1).build();
+        AdminInitiateAuthResult result = provider.adminInitiateAuth(req);
+    }
+}
--- src/main/java/Controller/LoginController.java
+++ src/main/java/Controller/LoginController.java
@@ -1,24 +1,35 @@
 package Controller;
 
+import Cognito.LoginCognito;
 import GUI.LoginPage;
+import com.amazonaws.services.cognitoidp.model.NotAuthorizedException;
+
+import java.lang.reflect.InvocationTargetException;
 
 public class LoginController {
     private LoginPage loginPage;
+    private LoginCognito loginCognito;
 
     public LoginController(LoginPage loginPage) {
         this.loginPage = loginPage;
+        this.loginCognito = new LoginCognito();
     }
 
     public void effettuaLogin(String username, String password) {
-        if (username.equals("admin") && password.equals("admin"))
+        try {
+            loginCognito.doLoginAdmin(username, password);
             openRatificaRecensioniPage();
-        else
+        }
+        catch (InvocationTargetException invocationTargetException) {
             loginPage.openDialogErroreLogin();
+        }
+        catch (NotAuthorizedException notAuthorizedException) {
+            loginPage.openDialogErroreLogin();
+        }
     }
 
     private void openRatificaRecensioniPage() {
         loginPage.loadNextScreen("RatificaRecensioni.fxml");
     }
-
 
 }
